7d3b375001d62393aa331b457a7ae7a5adf95dea
Klayton Souza <klayton.almeida@dce.ufpb.br>
2019-08-28 13:55:14 -0300

    Passando ate o teste 11

--- src/main/java/br/ufpb/dcx/aps/escalonador/FachadaEscalonador.java
+++ src/main/java/br/ufpb/dcx/aps/escalonador/FachadaEscalonador.java
@@ -1,80 +1,129 @@
 package br.ufpb.dcx.aps.escalonador;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class FachadaEscalonador {
-	
-	private int quantm;
+
+	private int quantum;
 	private int tick;
 	private TipoEscalonador tipoEscalonador;
-	private ArrayList<String> listaProcesso;
+	private Queue<String> listaProcesso;
 	private String rodando;
 	private ArrayList<String> processoBloqueado;
-	private String tempo;
+	private ArrayList<String> temp;
+	private String aFinalizar;
+	private int controlador;
+	private String resultado = "";
 
 	public FachadaEscalonador(TipoEscalonador tipoEscalonador) {
-		this.quantm = 3;
+		this.quantum = 3;
 		this.tick = 0;
-		this.rodando = "";
+		// this.rodando = "";
 		this.tipoEscalonador = tipoEscalonador;
-		this.listaProcesso = new ArrayList<String>();
+		this.listaProcesso = new LinkedList<String>();
 		this.processoBloqueado = new ArrayList<String>();
+		this.temp = new ArrayList<String>();
+
 	}
 
 	public FachadaEscalonador(TipoEscalonador roundrobin, int quantum) {
+		this.quantum = quantum;
+		this.tick = 0;
+		this.tipoEscalonador = roundrobin;
+		this.listaProcesso = new LinkedList<String>();
+		this.processoBloqueado = new ArrayList<String>();
+		this.temp = new ArrayList<String>();
+		
 	}
 
 	public String getStatus() {
-		if (this.listaProcesso.size() != 0 && tempo != listaProcesso.get(0)) {
-			return ("Escalonador " + this.tipoEscalonador + ";" 
-		+ "Processos: {Fila: " + this.listaProcesso.toString() + "};" 
-		+ "Quantum: " + this.quantm + ";" 
-		+ "Tick: " + this.tick);			
-		} else {
-			return ("Escalonador " + this.tipoEscalonador + ";" 
-			+ "Processos: {" + this.rodando + "};"
-			+ "Quantum: " + this.quantm + ";" 
-				+ "Tick: " + this.tick);
+
+		String resultado = "";
+
+		resultado += "Escalonador " + this.tipoEscalonador + ";";
+
+		resultado += "Processos: {";
+
+		if (rodando != null) {
+			resultado += "Rodando: " + this.rodando;
+
 		}
+		if (listaProcesso.size() > 0) {
+			if (rodando != null) {
+				resultado += ", ";
+			}
+			resultado += "Fila: " + this.listaProcesso.toString();
+			if(this.processoBloqueado.size() > 0) {
+				resultado += ", Bloqueados: " + this.processoBloqueado.toString();
+			}
+
+		}
+		resultado += "};Quantum: " + this.quantum + ";";
+
+		resultado += "Tick: " + this.tick;
+
+		return resultado;
 	}
 
 	public void tick() {
 		this.tick++;
-		if(this.listaProcesso.size() != 0){
-			this.rodando = "Rodando: "+this.listaProcesso.get(0);
-			tempo = listaProcesso.get(0);
-		
-		}else {
-			this.rodando = "";
-			this.tempo = "";
+		if (this.controlador > 0 && (this.controlador + this.quantum) == this.tick) {
+			this.listaProcesso.add(rodando);
+			this.rodando = this.listaProcesso.poll();
+			this.controlador = this.tick;
 		}
+
+		if (aFinalizar != null) {
+			if (this.rodando == this.aFinalizar) {
+				this.rodando = null;
+				
+			} else {
+				this.listaProcesso.remove(aFinalizar);
+			}
+			
+		}
+
+		if (this.rodando == null) {
+			if (this.listaProcesso.size() != 0) {
+				this.rodando = this.listaProcesso.poll();
+				if(listaProcesso.size() > 0) {
+					this.controlador = this.tick;
+				}
+				
+			}
+		}
+		if(this.controlador == 0 && this.rodando != null && listaProcesso.size() > 0) {
+			this.controlador = this.tick;
+		}
+
 	}
 
-
 	public void adicionarProcesso(String nomeProcesso) {
-		listaProcesso.add(nomeProcesso);
+		this.listaProcesso.add(nomeProcesso);
+
 	}
 
 	public void adicionarProcesso(String nomeProcesso, int prioridade) {
 	}
 
 	public void finalizarProcesso(String nomeProcesso) {
-		int nomeProcessoEncontrado = -1;
+		this.aFinalizar = nomeProcesso;
 
-		for(int k = 0; k<listaProcesso.size(); k++) {
-			if(listaProcesso.get(k).equals(nomeProcesso)) {
-				nomeProcessoEncontrado = k;				
-			}
-		}if(nomeProcessoEncontrado >= 0) {
-			listaProcesso.remove(nomeProcessoEncontrado);
-		}
 	}
 
 	public void bloquearProcesso(String nomeProcesso) {
+		for(int k = 0; k<listaProcesso.size(); k++) {
+			if(this.listaProcesso.size() > 0) {
+				this.listaProcesso.remove(nomeProcesso);
+				this.processoBloqueado.add(nomeProcesso);
+			}
+		}
+		
 	}
 
 	public void retomarProcesso(String nomeProcesso) {
 
 	}
-
 }
