ae9b78a1ee92d0cb5a487e85dd4ad5214dae87d9
Bianca Costa <bibixcosta1@gmail.com>
2019-09-24 18:15:57 -0300

    EscalonadorMaisCurtoPrimeiro rodando completo

--- src/main/java/br/ufpb/dcx/aps/escalonador/FachadaEscalonador.java
+++ src/main/java/br/ufpb/dcx/aps/escalonador/FachadaEscalonador.java
@@ -1,19 +1,29 @@
 package br.ufpb.dcx.aps.escalonador;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 public class FachadaEscalonador {
 
 	private int tick;
+
 	private int controlador;
 	private Queue<String> listaProcesso;
 	private ArrayList<String> processoBloqueado;
+
 	private List<String> fila = new ArrayList<String>();
 	private String rodando;
 	private List<Integer> filaDuracao = new ArrayList<Integer>();
 	private int duracaoRodando;
 	private int duracaoFixa;
+
+	private int quantum;
+
+	private TipoEscalonador tipoEscalonador;
+
+	private String processoParaSerFinalizado;
 
 	public FachadaEscalonador(TipoEscalonador tipoEscalonador) {
 		this.quantum = 3;
@@ -28,41 +38,33 @@ public class FachadaEscalonador {
 	}
 
 	public String getStatus() {
+
 		// MaisCurtoPrimeiro;Processos: {Fila: [P1]};Quantum: 0;Tick: 0
+
 		if (fila.size() == 0) {
 			if (rodando != null) {
 				return "Escalonador MaisCurtoPrimeiro;Processos: {Rodando: " + rodando + "};Quantum: 0;Tick: " + tick;
 			}
 			return "Escalonador MaisCurtoPrimeiro;Processos: {};Quantum: 0;Tick: " + tick;
-		}else if(rodando != null && fila.size()>0){
-			
+		} else if (rodando != null && fila.size() > 0) {
+
+			return "Escalonador MaisCurtoPrimeiro;Processos: {Rodando: " + rodando + ", Fila: " + fila
+					+ "};Quantum: 0;Tick: " + tick;
+
 		}
-		if (processoBloqueado.size() > 0)/* && listaProcesso.size()>0 ) */ {
-			if (listaProcesso.size() > 0) {
-				resultado += ", Bloqueados: " + this.processoBloqueado.toString();
-			} else {
-				resultado += "Bloqueados: " + this.processoBloqueado.toString();
-			}
-		}
+		return "Escalonador MaisCurtoPrimeiro;Processos: {Fila: " + fila + "};Quantum: 0;Tick: " + tick;
 
-		resultado += "};Quantum: " + this.quantum + ";";
-
-		resultado += "Tick: " + this.tick;
-
-		return resultado;
-//		return "Escalonador MaisCurtoPrimeiro;Processos: {Fila: " + fila + "};Quantum: 0;Tick: " + tick;
-		
 	}
 
 	public TipoEscalonador getTipoEscalonador() {
 		return tipoEscalonador;
+
 	}
 
 	public void setTipoEscalonador(TipoEscalonador tipoEscalonador) {
 		this.tipoEscalonador = tipoEscalonador;
 	}
 
-	
 	public void tick() {
 		tick++;
 		if (fila.size() > 0) {
@@ -77,32 +79,39 @@ public class FachadaEscalonador {
 			if (fila.size() > 0) {
 				rodando = fila.remove(0);
 				duracaoRodando = filaDuracao.remove(0);
-			}
-			else {
+			} else {
 				rodando = null;
 				duracaoRodando = 0;
 			}
-			if(duracaoRodando > 0) {
+			if (duracaoRodando > 0) {
 				duracaoFixa = tick + duracaoRodando;
-				
+
 			}
 		}
 	}
 
 	public void adicionarProcesso(String nomeProcesso) {
-		if (listaProcesso.contains(nomeProcesso)) {
-			throw new EscalonadorException();
-		}
+
+		
 		this.listaProcesso.add(nomeProcesso);
 
+		if (listaProcesso.contains(nomeProcesso)) {
+			throw new EscalonadorException();
+		}else {
+			throw new EscalonadorException();
+		}	
+		
 	}
 
 	public void adicionarProcesso(String nomeProcesso, int prioridade) {
+
+		
+
 		if (listaProcesso.contains(nomeProcesso)) {
 			throw new EscalonadorException();
 		}
 		this.listaProcesso.add(nomeProcesso);
-		
+
 	}
 
 	public void finalizarProcesso(String nomeProcesso) {
@@ -119,16 +128,45 @@ public class FachadaEscalonador {
 	}
 
 	public void adicionarProcessoTempoFixo(String nomeProcesso, int duracao) {
-		
-		if(fila.size()==0) {
+
+		if (fila.contains(nomeProcesso) || nomeProcesso == null) {
+			throw new EscalonadorException();
+
+		}
+		if (duracao < 1) {
+			throw new EscalonadorException();
+		}
+		int maisCurto = Integer.MAX_VALUE;
+
+		if (fila.size() == 0) {
 			fila.add(nomeProcesso);
 			filaDuracao.add(duracao);
-		}
-		else {
-			for(int k = 0; duracao<k; k++) {
-				fila.add(0, nomeProcesso);
-				filaDuracao.add(0, duracao);
+		} else {
+
+			int menorPosicao = 0;
+
+			fila.add(nomeProcesso);
+			filaDuracao.add(duracao);
+			for (int i = 0; i < filaDuracao.size(); i++) {
+				if (filaDuracao.get(i) < maisCurto) {
+					maisCurto = filaDuracao.get(i);
+					menorPosicao = i;
+				}
+
 			}
+			if (menorPosicao > 0) {
+				String processoMenor = fila.remove(menorPosicao);
+				int processoMenorTempo = filaDuracao.remove(menorPosicao);
+				fila.add(0, processoMenor);
+				filaDuracao.add(0, processoMenorTempo);
+
+				for (int k = 0; duracao < k; k++) {
+					fila.add(0, nomeProcesso);
+					filaDuracao.add(0, duracao);
+
+				}
+			}
+
 		}
 	}
 }
